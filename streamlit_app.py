9bbb51a439eb046ac071aa29b5597aaf429e9dd8
TaiwoA22 <107038388+TaiwoA22@users.noreply.github.com>
2022-06-14 13:58:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ streamlit.text('🥭Kale, Spinach & Rocket Smoothie')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.header('Build your own smoohtie')
 streamlit.dataframe(my_fruit_list)
