4e901cc537ef169035ecf0cf475c1dab73714db7
TaiwoA22 <107038388+TaiwoA22@users.noreply.github.com>
2022-06-14 13:41:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3& Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
